0c1234a936688dbf44d347b3dd001278517512ba
OGOZ111 <142310217+OGOZ111@users.noreply.github.com>
2024-05-05 20:33:41 +0300

    changes according the requirements

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
-const backButton = document.querySelector('#backToTop');
-const mobButton = document.querySelector('.mobile')
-const nav = document.querySelector('nav ul')
-const menuItems = document.querySelectorAll('nav ul li a')
-const header = document.querySelector('header')
-const modalbutton = document.querySelector('#modalbutton')
-const overlay = document.querySelector('.overlay')
-const closebutton = document.querySelector('.close')
+document.addEventListener("DOMContentLoaded", function () {
+  const backButton = document.querySelector("#backToTop");
+  const mobButton = document.querySelector(".mobile");
+  const nav = document.querySelector("nav ul");
+  const menuItems = document.querySelectorAll("nav ul li a");
+  const header = document.querySelector("header");
+  const modalbutton = document.querySelector("#modalbutton");
+  const overlay = document.querySelector(".overlay");
+  const closebutton = document.querySelector(".close");
 
-window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-// w3 schools, 2 different code for different browser engines. 
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
       backButton.style.display = "block";
     } else {
       backButton.style.display = "none";
     }
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        header.classList.add('bg');
-      } else {
-        header.classList.remove('bg');
-      }
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      header.classList.add("bg");
+    } else {
+      header.classList.remove("bg");
+    }
   }
 
-const getToTop = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+  const getToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-const mobMenu = () => {
+  const mobMenu = () => {
     for (const item of menuItems) {
-        item.addEventListener('click', mobMenu)
+      item.addEventListener("click", mobMenu);
     }
 
-    if (nav.classList.contains('responsive')){
-        nav.classList.remove('responsive')
-    } else {nav.classList.add('responsive')}
+    if (nav.classList.contains("responsive")) {
+      nav.classList.remove("responsive");
+    } else {
+      nav.classList.add("responsive");
+    }
+  };
 
-}
+  const modshow = () => {
+    overlay.classList.toggle("visible");
+  };
 
-const modshow = () => {
-  overlay.classList.toggle('visible')
-}
-  
-
-
-closebutton.addEventListener('click', modshow)
-modalbutton.addEventListener('click', modshow)
-mobButton.addEventListener('click', mobMenu)
-backButton.addEventListener('click', getToTop)
+  closebutton.addEventListener("click", modshow);
+  modalbutton.addEventListener("click", modshow);
+  mobButton.addEventListener("click", mobMenu);
+  backButton.addEventListener("click", getToTop);
+});
